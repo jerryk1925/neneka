1a1b264bcf8a61ff56f51a06bd72c868c76dd02d
r.himiranov <r.himiranov@smartweb.com.ua>
2020-09-21 17:09:53 +0300

    fix

--- neneka/app.py
+++ neneka/app.py
@@ -1,9 +1,12 @@
 import aiopg.sa
 
+from sqlalchemy import create_engine, MetaData
 from aiohttp import web
+from sqlalchemy.schema import CreateTable, DropTable
 
 from neneka.routes import init_routes
 from neneka.utils import get_config
+from neneka.db import question, choice
 
 
 def init_config(app: web.Application, argv=None) -> None:
@@ -15,7 +18,7 @@ async def init_database(app: web.Application) -> None:
     config = app['config']['postgres']
     print(config)
     engine = await aiopg.sa.create_engine(**config)
-    print(engine)
+
     app['db'] = engine
 
 async def close_database(app: web.Application) -> None:
@@ -27,8 +30,8 @@ def init_app(argv=None) -> web.Application:
     app = web.Application()
     init_config(app, argv)
     init_routes(app)
-    app.on_startup.extend([init_database])
-    app.on_cleanup.extend([close_database])
+    app.on_startup.append(init_database)
+    app.on_cleanup.append(close_database)
     return app
 
 
--- neneka/views.py
+++ neneka/views.py
@@ -1,6 +1,15 @@
 from aiohttp import web
-
+from sqlalchemy.schema import CreateTable, DropTable
+from neneka.db import question, choice
 
 async def index(request):
-  print(request)
+  print(request.app['db'])
+  async with request.app['db'].acquire() as conn:
+    async for row in conn.execute(question.select()):
+      print(row.id, row.name)
+    # print(conn)
+    # await conn.execute(DropTable(question))
+    # await conn.execute(CreateTable(question))
+    # await conn.execute(question.insert().values(id=2, name='qwe'))
+
   return web.Response(text="Hello worasdasldfgdgd")
